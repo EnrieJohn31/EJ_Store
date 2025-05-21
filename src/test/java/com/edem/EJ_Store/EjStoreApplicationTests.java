005165a18b5acccc3c745a1af227253a9782222e
EnrieJohn31 <enriejohnedem@gmail.com>
2025-05-21 20:56:17 +0800

    Add: Controller and Static HTML

---
+++ src/test/java/com/edem/EJ_Store/EjStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edem.EJ_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
